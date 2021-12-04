2015e9fc9432d487765bf5be28d2168748593bf5
Nahidjc <nahidjc80@gmail.com>
2021-12-04 12:42:55 +0600

    sun draw

--- main.cpp
+++ main.cpp
@@ -1,5 +1,50 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include <windows.h>
+#include<math.h>>
+# define PI           3.14159265358979323846
+
+
+#include <stdlib.h>
+#include <math.h>
+
+////Position for object
+float sunX = 50;
+float sunY = 50;
+
+//////////////circle drawing function
+void DrawCircle(float cx, float cy, float r, int num_segments)
+{
+
+    glBegin(GL_TRIANGLE_FAN);
+    for (int i = 0; i < num_segments; i++)
+    {
+        float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);//get current angle
+
+        float x = r * cosf(theta);//calculate x
+        float y = r * sinf(theta);//calculate y
+
+        glVertex2f(x + cx, y + cy);//output vertex
+
+    }
+    glEnd();
+    glFlush();
+}
+
+
+//Draw Sun
+void drawSun(int nasar)
+{
+    glColor3ub (250, 250, 0);
+    DrawCircle(180, 180, 8, 500000);
+
+    glEnd();
+    glFlush();
+
+}
+
+
+
 
 
 
@@ -10,7 +55,44 @@ void display(void)
     /* clear all pixels */
     glClear(GL_COLOR_BUFFER_BIT);
     int i,j;
-//HIghway road
+    drawSun(1);
+    //School design
+    glBegin(GL_QUADS);
+    glColor3ub(115, 112, 112);
+
+    glVertex3f(130,140,0);
+    glVertex3f(145,140,0);
+    glVertex3f(145,142,0);
+    glVertex3f(130,142,0);
+    glEnd();
+
+    for(j=0; j<12; j=j+4)
+    {
+
+        glBegin(GL_QUADS);
+        glColor3ub(49, 36, 82);
+
+        glVertex3f(132+j,126,0);
+        glVertex3f(134+j,126,0);
+        glVertex3f(134+j,130,0);
+        glVertex3f(132+j,130,0);
+        glEnd();
+
+    }
+
+    //red mark
+    glBegin(GL_QUADS);
+    glColor3ub(232, 19, 19);
+
+    glVertex3f(130,125,0);
+    glVertex3f(145,125,0);
+    glVertex3f(145,140,0);
+    glVertex3f(130,140,0);
+    glEnd();
+
+
+
+    //Highway road
     glBegin(GL_QUADS);
     glColor3ub(152, 156, 153);
 
@@ -20,7 +102,7 @@ void display(void)
     glVertex3f(0,100,0);
     glEnd();
 
-
+    // Road divider line
     for(i = 0; i <= 195; i=i+15)
     {
         glBegin(GL_QUADS);
@@ -33,8 +115,8 @@ void display(void)
         glEnd();
     }
 
-
-    for(i=0;i<=37; i=i+8)
+    // Zebra Crossing
+    for(i=0; i<=37; i=i+8)
     {
 
         glBegin(GL_QUADS);
@@ -99,14 +181,12 @@ int main(int argc, char** argv)
 
     glutInit(&argc, argv);
     glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+glutInitWindowSize (840, 680);
+glutInitWindowPosition (100, 150);
 
-
-    glutInitWindowSize (1400, 800);
-    glutInitWindowPosition (100, 100);
     glutCreateWindow ("School Scenario");
     init ();
     glutDisplayFunc(display);
-
 
 
     glutMainLoop();
